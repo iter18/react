9f3e25f8db19774edcce982ead507082d2078e29
iter18 <ti0911342o@gmail.com>
2025-12-01 16:28:16 -0600

    Se agrega componente de prueba

---
+++ 2-intro-react/src/components/HelloWorld.jsx
@@ -0,0 +1,9 @@
+export function HelloWorld() {
+    return (
+            <>
+                <h1>Hello, World!</h1>
+                <div>que tal!!</div>
+            </>
+
+    );
+}
--- 2-intro-react/src/main.jsx
+++ 2-intro-react/src/main.jsx
@@ -1,24 +1,11 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { HelloWorld } from './components/HelloWorld.jsx'
 
-//const h1 = React.createElement('h1', null, 'Hello, world!');
-//forma mas compleja de crear elementos en react
-const h1 = React.createElement('ul', null, 
-                                  React.createElement('li', null, 'item 1'), 
-                                  React.createElement('li', null, 'item 2'), 
-                                  React.createElement('li', null, 'item 3'));
-
-//Forma usando JSX
-const h2 = <div ><ul>
-                  <li>item 1</li>
-                  <li>item 2</li>
-                </ul></div>
 
 createRoot(document.getElementById('root')).render(
-  //<StrictMode>
-    //<App />
-  //</StrictMode>,
-  h2
+  <StrictMode>
+    <HelloWorld />
+  </StrictMode>
+ 
 )
