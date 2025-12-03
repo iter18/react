03fdee79c6933ec2418bf63743550b6d7a445577
iter18 <ti0911342o@gmail.com>
2025-12-03 17:36:38 -0600

    se agregan props y valores a componente hijo

--- 2-intro-react/src/components/HelloWorld.jsx
+++ 2-intro-react/src/components/HelloWorld.jsx
@@ -1,16 +1,20 @@
 //use 'props' to receive properties from the parent component
 //export function HelloWorld(props) {
 //destructure the props to get the values directly
-export const HelloWorld = ({user, id}) => {
-    const name = 'Juan';
+//asignar valor por defecto a title, pasar objetos y valores numericos, de esta menera se recibe cualquier tipo de dato por props
+export const HelloWorld = ({user, id, title = 'default title',numero,direccion}) => {
+    
+    console.log(title)
 
     return (
             <>
-                <h1>Hello, World!</h1>
+                <h1>{title}</h1>
                 {/*Usando props*/}
                 {/*<div>que tal, {props.user}!! con el id {props.id}</div>*/}
                 {/*Usando destructuring*/}
                 <div>que tal, {user}!! con el id {id}</div>
+                <div>numero: {numero + 3}</div>
+                <div>direccion: {direccion.calle}, {direccion.number} (id: {direccion.id})</div>
             </>
 
     );
--- 2-intro-react/src/main.jsx
+++ 2-intro-react/src/main.jsx
@@ -5,7 +5,7 @@ import { HelloWorld } from './components/HelloWorld.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelloWorld user={"Juan"} id='1'/>
+    <HelloWorld user={"Juan"} id={"1"} numero={ 2 } direccion = {{ id: '10', calle:' elm street', number:20}}/>
   </StrictMode>
  
 )
